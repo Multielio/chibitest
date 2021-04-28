0ff82926d876cf4b3eef749d6482500f4e9f6930
elio <eliov@hotmail.fr>
2021-04-28 11:14:53 +0200

    init

---
+++ .dep/crt1.o.d
@@ -0,0 +1,37 @@
+build/obj/crt1.o: ../../../os/common/startup/ARMCMx/compilers/GCC/crt1.c \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ ../../../os/common/startup/ARMCMx/devices/STM32F4xx/cmparams.h \
+ ../../../os/hal/boards/ST_NUCLEO64_F446RE/board.h \
+ ../../../os/common/ext/ST/STM32F4xx/stm32f4xx.h \
+ ../../../os/common/ext/ST/STM32F4xx/stm32f446xx.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/core_cm4.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_version.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_compiler.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_gcc.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/mpu_armv7.h \
+ ../../../os/common/ext/ST/STM32F4xx/system_stm32f4xx.h
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
+
+../../../os/common/startup/ARMCMx/devices/STM32F4xx/cmparams.h:
+
+../../../os/hal/boards/ST_NUCLEO64_F446RE/board.h:
+
+../../../os/common/ext/ST/STM32F4xx/stm32f4xx.h:
+
+../../../os/common/ext/ST/STM32F4xx/stm32f446xx.h:
+
+../../../os/common/ext/ARM/CMSIS/Core/Include/core_cm4.h:
+
+../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_version.h:
+
+../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_compiler.h:
+
+../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_gcc.h:
+
+../../../os/common/ext/ARM/CMSIS/Core/Include/mpu_armv7.h:
+
+../../../os/common/ext/ST/STM32F4xx/system_stm32f4xx.h:
---
+++ main.cpp
@@ -0,0 +1,130 @@
+/*
+    ChibiOS - Copyright (C) 2006..2018 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "ch.h"
+#include "hal.h"
+#include "rt_test_root.h"
+#include "oslib_test_root.h"
+#include "frenquency.h"
+#include "XNucleoIHM02A1.h"
+
+
+
+XNucleoIHM02A1 *x_nucleo_ihm02a1;
+L6470 **motors;
+
+/* Initialization parameters of the motors connected to the expansion board. */
+L6470_init_t L6470_init[L6470DAISYCHAINSIZE] = {
+    /* First Motor. */
+    {
+        9.0,                           /* Motor supply voltage in V. */
+        400,                           /* Min number of steps per revolution for the motor. */
+        1.7,                           /* Max motor phase voltage in A. */
+        3.06,                          /* Max motor phase voltage in V. */
+        300.0,                         /* Motor initial speed [step/s]. */
+        500.0,                         /* Motor acceleration [step/s^2] (comment for infinite acceleration mode). */
+        500.0,                         /* Motor deceleration [step/s^2] (comment for infinite deceleration mode). */
+        992.0,                         /* Motor maximum speed [step/s]. */
+        0.0,                           /* Motor minimum speed [step/s]. */
+        602.7,                         /* Motor full-step speed threshold [step/s]. */
+        3.06,                          /* Holding kval [V]. */
+        3.06,                          /* Constant speed kval [V]. */
+        3.06,                          /* Acceleration starting kval [V]. */
+        3.06,                          /* Deceleration starting kval [V]. */
+        61.52,                         /* Intersect speed for bemf compensation curve slope changing [step/s]. */
+        392.1569e-6,                   /* Start slope [s/step]. */
+        643.1372e-6,                   /* Acceleration final slope [s/step]. */
+        643.1372e-6,                   /* Deceleration final slope [s/step]. */
+        0,                             /* Thermal compensation factor (range [0, 15]). */
+        3.06 * 1000 * 1.10,            /* Ocd threshold [ma] (range [375 ma, 6000 ma]). */
+        3.06 * 1000 * 1.00,            /* Stall threshold [ma] (range [31.25 ma, 4000 ma]). */
+        StepperMotor::STEP_MODE_1_128, /* Step mode selection. */
+        0xFF,                          /* Alarm conditions enable. */
+        0x2E88                         /* Ic configuration. */
+    },
+
+    /* Second Motor. */
+    {
+        9.0,                           /* Motor supply voltage in V. */
+        400,                           /* Min number of steps per revolution for the motor. */
+        1.7,                           /* Max motor phase voltage in A. */
+        3.06,                          /* Max motor phase voltage in V. */
+        300.0,                         /* Motor initial speed [step/s]. */
+        500.0,                         /* Motor acceleration [step/s^2] (comment for infinite acceleration mode). */
+        500.0,                         /* Motor deceleration [step/s^2] (comment for infinite deceleration mode). */
+        992.0,                         /* Motor maximum speed [step/s]. */
+        0.0,                           /* Motor minimum speed [step/s]. */
+        602.7,                         /* Motor full-step speed threshold [step/s]. */
+        3.06,                          /* Holding kval [V]. */
+        3.06,                          /* Constant speed kval [V]. */
+        3.06,                          /* Acceleration starting kval [V]. */
+        3.06,                          /* Deceleration starting kval [V]. */
+        61.52,                         /* Intersect speed for bemf compensation curve slope changing [step/s]. */
+        392.1569e-6,                   /* Start slope [s/step]. */
+        643.1372e-6,                   /* Acceleration final slope [s/step]. */
+        643.1372e-6,                   /* Deceleration final slope [s/step]. */
+        0,                             /* Thermal compensation factor (range [0, 15]). */
+        3.06 * 1000 * 1.10,            /* Ocd threshold [ma] (range [375 ma, 6000 ma]). */
+        3.06 * 1000 * 1.00,            /* Stall threshold [ma] (range [31.25 ma, 4000 ma]). */
+        StepperMotor::STEP_MODE_1_128, /* Step mode selection. */
+        0xFF,                          /* Alarm conditions enable. */
+        0x2E88                         /* Ic configuration. */
+    }
+};
+
+
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
+
+  /*
+   * Activates the serial driver 2 using the driver default configuration.
+   */
+  sdStart(&SD2, NULL);
+
+
+
+  // LIB MOTEUR TEST
+  x_nucleo_ihm02a1 = new XNucleoIHM02A1(&L6470_init[0], &L6470_init[1], PAL_LINE(GPIOC, 1), PAL_LINE(GPIOC, 0), PAL_LINE(GPIOB, 5), PAL_LINE(GPIOA, 4));
+  motors = x_nucleo_ihm02a1->get_components();
+  L6470_init[0].step_sel = StepperMotor::STEP_MODE_1_128;
+  L6470_init[1].step_sel = StepperMotor::STEP_MODE_1_128;
+  motors[0]->prepare_run(StepperMotor::BWD, 50);
+  motors[1]->prepare_run(StepperMotor::FWD, 50);
+  x_nucleo_ihm02a1->perform_prepared_actions();
+  //
+
+
+  /*
+   * Normal main() thread activity
+   */
+  while (true) {
+    
+    chThdSleepMilliseconds(500);
+  }
+}
